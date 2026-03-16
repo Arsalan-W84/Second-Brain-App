63212ba64b5c048d2187209056d68b22676c2c41
Arsalan-W84 <arsalanwahid0804@gmail.com>
2026-03-16 18:37:04 +0000

    SIGNUP LOGIN AND AUTH ADDED

---
+++ src/schema.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+import { required } from 'zod/mini';
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username : {type : String , required:true},
+    password : {type : String , required:true}
+});
+
+const ContentSchema = new Schema({
+    link : String,
+    type : {type : String, enum:['image' , 'video' , 'article' , 'tweet']} ,
+    title: String,
+    tags:  [{type : Schema.Types.ObjectId , ref : 'Tag' } ],
+    userId : {type : Schema.Types.ObjectId , ref : 'User' }
+
+})
+
+const TagSchema = new Schema({
+    title : String
+});
+
+const LinkSchema = new Schema({
+    hash : String,
+    userId : {type : Schema.Types.ObjectId , ref : 'User' , required:true}
+});
+
+
+export const UserModel = mongoose.model('User' , UserSchema);
+export const ContentModel = mongoose.model('Content' , ContentSchema);
+export const TagModel = mongoose.model('Tag' , TagSchema);
+export const LinkModel =mongoose.model('Links' , LinkSchema);
+
+
+
+
