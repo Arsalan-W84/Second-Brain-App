ebb2ae44b75f6f70d4e2395df4c00df7fead73a9
Arsalan-W84 <arsalanwahid0804@gmail.com>
2026-08-03 16:06:08 +0000

    Fix BE for deployment

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,12 @@ dotenv.config();
 import express from 'express';
 const app = express();
 import cors from 'cors'
-
 import mongoose from 'mongoose';
+
+mongoose.connect(process.env.DB_LINK as string)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('MongoDB connection error:', err));
+
 
 import authRoutes from './routes/authRoutes.js';
 import contentRoutes from './routes/contentRoutes.js'
@@ -16,12 +20,17 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1/auth" , authRoutes);//AUTH ENDPOINTS
-
 app.use("/api/v1/content" , contentRoutes);//CRUD ENPOINTS
-
 app.use("/api/v1/brain" , brainRoutes);// SHARE ENDPOINTS
 
-app.listen(3000, async() => {
-  await mongoose.connect(process.env.DB_LINK);
-  console.log('Server listening on http://localhost:3000');
-});
+
+// Only run app.listen() locally
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
+}
+
+
+export default app;
